ec1d7f2cb863beb6e46385736c2886829d19844e
Easrib <easrib.sharik01@gmail.com>
2022-05-24 19:55:34 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -11,7 +11,19 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jziborc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        await client.connect();
+        console.log('data base connected');
 
+    }
+    finally {
+
+    }
+
+};
+
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
