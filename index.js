1b5060b9bff32e405e32f9c7dfb49debecdf7c8e
Easrib <easrib.sharik01@gmail.com>
2022-05-27 09:55:46 +0600

    jwt verify

--- index.js
+++ index.js
@@ -12,6 +12,21 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jziborc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'You are not authorized' })
+    };
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+
+}
 async function run() {
     try {
         await client.connect();
@@ -36,11 +51,17 @@ async function run() {
             const result = await reviewCollection.find(query).toArray();
             res.send(result)
         })
-        app.get('/order', async (req, res) => {
+        app.get('/order', verifyJWT, async (req, res) => {
             const email = req.query.email;
-            const query = { email: email };
-            const result = await orderCollection.find(query).toArray();
-            res.send(result)
+            const decodedEmail = req.decoded.email;
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const result = await orderCollection.find(query).toArray();
+                return res.send(result)
+            }
+            else {
+                return res.status(403).send({ message: 'Forbidden access' })
+            }
         })
         app.post('/review', async (req, res) => {
             const review = req.body;
@@ -63,7 +84,7 @@ async function run() {
             };
             const result = await profileCollection.updateOne(filter, updateDoc, options);
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-            res.send({result, token})
+            res.send({ result, token })
         })
 
 
